fe0da846bec38f8cbf65b4400c6a8fdf7abc6491
exteriorduke319 <yarnevictanner@gmail.com>
2023-10-30 09:31:17 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,21 @@ public class Main {
         for (int i = 0; i <= 30; i++) {
             System.out.print(i + " ");
         }
+
         System.out.println();
+
         for (int i = 30; i >= 0; i--) {
             System.out.print(i + " ");
         }
+
         System.out.println();
+
         for (int i = 3; i <= 18; i+=3) {
             System.out.print(i + " ");
         }
+
         System.out.println();
+
         for (int i = 10; i >= 0; i-=2) {
             System.out.print(i + " ");
         }
